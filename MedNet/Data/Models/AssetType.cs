1fab714c452ea04c9b8da3c209609fb1301862b4
jshercli <josh_shercliffe@sfu.ca>
2020-04-08 17:10:57 -0700

    Added framework for data folder

--- MedNet/Data/Models/AssetType.cs
+++ MedNet/Data/Models/AssetType.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace MedNet.Data.Models
+﻿namespace MedNet.Data.Models
 {
-        public enum AssetType: int
-        {
-            Doctor = 0,
-            Patient = 1,
-            DoctorNote = 2
-        };
+    public enum AssetType: int
+    {
+        Doctor = 0,
+        Patient = 1,
+        DoctorNote = 2
+    };
 }
--- MedNet/Data/Models/Models/Asset.cs
+++ MedNet/Data/Models/Models/Asset.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace MedNet.Data.Models.Models
+﻿namespace MedNet.Data.Models.Models
 {
     public class Asset
     {
