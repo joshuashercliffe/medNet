563dabe2184003ff4909fa0320b299b25fd0c0a1
Jacob <jcw41@sfu.ca>
2020-07-17 16:04:35 -0700

    partially implemented PatientEditProfile, needs debug work

--- MedNet/Models/PatientEditProfileViewModel.cs
+++ MedNet/Models/PatientEditProfileViewModel.cs
@@ -1,18 +1,49 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using MedNet.Data.Models.Models;
+using System;
 
 namespace MedNet.Models
 {
     public class PatientEditProfileViewModel
     {
-        public string address { get; set; }
-        public string phone { get; set; }
-        public double weight { get; set; }
-        public List<string> allergies { get; set; }
-        public List<string> daily_meds { get; set; }
-        public List<string> med_hist { get; set; }
-        public string emer_name { get; set; }
-        public string emer_phone { get; set; }
-        public string emer_rel { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public Prov Province { get; set; }
+        public string Postal { get; set; }
+
+        // Emergency contact info
+        public string emerFirstName { get; set; }
+        public string emerLastName { get; set; }
+        public string emerPhone { get; set; }
+        public string emerAddress { get; set; }
+        public string emerCity { get; set; }
+        public Prov emerProvince { get; set; }
+        public string emerPostal { get; set; }
+        public enum Prov
+        {
+            Alberta,
+            BritishColumbia,
+            Manitoba,
+            NewBrunswick,
+            NewfoundlandAndLabrador,
+            NovaScotia,
+            Ontario,
+            PrinceEdwardIsland,
+            Saskatchewan,
+            Quebec,
+            NorthWestTerritories,
+            Nunavut,
+            Yukon
+        }
+        public string Relationship { get; set; }
+        public string[] Relationships = new[] { "Partner", "Parent", "Sibling", "Relative", "Friend", "Other" }; 
+
     }
 }
