006e3caec740b50cb5f3733d45e9139c7ca7f379
jacob <jacob.wongg@gmail.com>
2020-07-30 13:04:03 -0700

    added height, weight, age, DoB to patient edit profile and overview pages

--- MedNet/Data/Models/Models/PatientCredentials.cs
+++ MedNet/Data/Models/Models/PatientCredentials.cs
@@ -27,7 +27,11 @@ namespace MedNet.Data.Models.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
         public string Email { get; set; }
+        public int Height { get; set; }
+        public double Weight { get; set; }
         public string Phone { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
